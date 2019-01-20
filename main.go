9a4653a6873d3c1a8cdb965b6e92d991b14e77a6
Ali Hassaan Mughal <amughal.bscs16seecs@seecs.edu.pk>
2019-01-20 14:26:48 +0500

    Touch up of the basic program

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
+//Make a executable package.
 package main
 
+//Import format library for printing
 import "fmt"
 
+//main function (a must in executable package)
 func main() {
 	fmt.Println("Hi There")
 }
