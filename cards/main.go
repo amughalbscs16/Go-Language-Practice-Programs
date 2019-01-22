591da2f408610eefe7c7ce7120dd892a37e0c1cb
Ali Hassaan Mughal <amughal.bscs16seecs@seecs.edu.pk>
2019-01-23 01:31:09 +0500

    Data Type Variables in Go

---
+++ cards/main.go
@@ -0,0 +1,13 @@
+package main
+
+import fmt
+func main()
+{
+	//Static Type of Data in Go.
+	//var card string = "Ace of Space" (Bottom line means same)
+	card := "Ace of Spades" //:= Only for declaratio and initialization;
+	card = "Five of Diamods"
+		
+
+	fmt.Println(card)
+}
